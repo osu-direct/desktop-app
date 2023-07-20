29d8d41d675019445f7c1a14de89391678213a9f
HorizonCode <horizoncode88@gmail.com>
2023-07-20 19:06:36 +0200

    fix ipc settings handler

--- src/main.ts
+++ src/main.ts
@@ -41,14 +41,18 @@ function createWindow() {
     if (i.endsWith("/settings")) {
       e.preventDefault();
       //TODO: open settings window
+      const point = screen.getCursorScreenPoint();
+      const { bounds } = screen.getDisplayNearestPoint(point);
+
       settingsWindow = new BrowserWindow({
         parent: mainWindow,
+        x: bounds.x,
+        y: bounds.y,
         icon: path.join(__dirname, "..", "assets", "logo.png"),
         modal: true,
         width: 500,
         height: 200,
         title: "Settings",
-        alwaysOnTop: true,
         type: "dialog",
         minimizable: false,
         maximizable: false,
@@ -60,20 +64,22 @@ function createWindow() {
           nodeIntegration: true,
         },
       });
-      ipcMain.handle("browse-folder", async () => {
-        const yes = await dialog.showOpenDialog(settingsWindow, {
-          properties: ['openDirectory'],
-        });
-        console.log(yes);
-        if(yes.canceled || yes.filePaths.length <= 0) return "";
-        return yes.filePaths[0];
-      })
+      settingsWindow.center();
       settingsWindow.show();
       settingsWindow.loadFile(path.join(__dirname, "..", "html", "settings.html"));
     } else if (i.startsWith("https://osu.direct/d/")) {
       //TODO: download handling
     }
   });
+
+  ipcMain.handle("browse-folder", async () => {
+    const yes = await dialog.showOpenDialog(settingsWindow, {
+      properties: ['openDirectory'],
+    });
+    console.log(yes);
+    if (yes.canceled || yes.filePaths.length <= 0) return "";
+    return yes.filePaths[0];
+  })
 
   mainWindow.webContents.on("did-finish-load", () => mainWindow.show())
 
